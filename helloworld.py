a51b6ac71739c3b23e4378675e01d2d4b08e6cb9
yogi <yogi@gmail.cOm>
2023-06-24 17:27:14 +0530

    deploy to python

--- helloworld.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-"""Top-level script to invoke helloworld implementation."""
-
-import sys
-import helloworld.main
-
-if __name__ == '__main__':
-    sys.exit(helloworld.main.main())
--- helloworld/__init__.py
+++
@@ -1,5 +0,0 @@
-def __read_version_txt():
-  import pkgutil
-  return pkgutil.get_data('helloworld', 'VERSION.txt').decode('utf-8').strip()
-
-__version__ = __read_version_txt()
